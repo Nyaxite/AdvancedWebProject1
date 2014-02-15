eff9c7c4da223aaef2eda3b3d44b27f91ab73e89
Michael Burnie <nyaxite@outlook.com>
2014-02-15 15:09:51 -0500

    Version 1.2
    - Added owl carousel
    - Footer includes link to desktop version
    - Added back button to home page only as placeholder
    - Moved content to appropriate pages
    - Added mobile-specific JS and CSS files

---
+++ files/js/mobile-jquery.js
@@ -0,0 +1,26 @@
+﻿/*
+File: jquery-main.css
+Author: Michael Burnie
+Website: www.michaelburnie.com
+Description: This is the jQuery and JavaScript code for my mobile website. Other plugins are generally called from this page.
+*/
+$(document).ready(function() {
+ 
+	$("#owl-demo").owlCarousel({
+
+		autoPlay: 3000, //Set AutoPlay to 3 seconds
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		//singleItem:true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
+	});
+ 
+});
